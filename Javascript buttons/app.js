f39769a60007b1d2edb997a6bdb6adf4a197dc54
Ibrahim Kashif <111134217+ibrahim08043@users.noreply.github.com>
2022-10-01 11:06:40 -0700

    Add files via upload

---
+++ Javascript buttons/app.js
@@ -0,0 +1,86 @@
+// function mus(tony){
+// var a = document.getElementById(typ)
+//     console.log(typ)
+//     typ.style.display="none"
+// }
+
+// function muj(bob){
+//     var a = document.getElementById(circ)
+//         console.log(circ)
+//     circ.style.display="none"
+//     }
+    
+    
+//     function hus(javes){
+//         var a = document.getElementById(tri)
+//             console.log(tri)
+//             tri.style.display="none"
+//         }
+        
+//         function fu(){
+//             var c = document.getElementById(copy)
+//             console.log(copy)
+         
+//         }
+
+// function wah(){
+//     var a = document.getElementById(typ)
+//     console.log(typ)
+
+// }
+
+// function wa(){
+//     var a = document.getElementById(circ)
+//     console.log(circ)
+//     circ = ".
+// }
+
+// function w(){
+
+//     var a = document.getElementById(tri)
+//     console.log(tri)
+
+// }
+// function abc(){
+//     var a = document.getElementById()
+//     .style.display="none"
+// }
+
+
+
+function mus( tony){
+
+    var a = document.getElementById(tri)
+    console.log(tri)
+    tri.src="images/download.png"
+
+}
+function muj(){
+    var a = document.getElementById(tri)
+    tri.src="images/img 2.jfif"
+}
+
+function hus(){
+    var a = document.getElementById(tri)
+    tri.src="images/img.png"
+}
+
+
+// function mus(){
+// var a = document.getElementById(tri)
+// tri.src="images/download.png"
+// }
+
+// function zxc(){
+
+
+//    var a = document.getElementById(tri)
+//     tri.src="images/img 2.jfif"
+
+// }
+
+// function uio(){
+//     var a = document.getElementById(tri)
+//     tri.src="images/img.png"
+
+// }
